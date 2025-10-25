8752aee06389acded8b4eb070ce0437a40deecb4
Fan Jianhui <66809633+fmhgit@users.noreply.github.com>
2025-10-25 19:20:03 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
   title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/ccbikai/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
+  email: 'dsman@qq.com',
+  github: 'https://github.com/fmhgit/sink',
+  twitter: 'https://s.isee.qzz.io',
+  telegram: 'https://s.isee.qzz.io',
+  mastodon: 'https://s.isee.qzz.io',
+  blog: 'https://s.isee.qzz.io',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
